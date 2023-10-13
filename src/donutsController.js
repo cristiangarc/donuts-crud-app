dd1f81494c92b09e9aa393a1986ad01d322531a9
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-13 15:19:23 -0400

    added functionality to the edit function; searched for the donut's index using an id and the donuts array as parameters; updated the name, price and inStock properties and returned the modified donuts array

--- src/donutsController.js
+++ src/donutsController.js
@@ -14,7 +14,22 @@ const create = (donuts, name = '') => {
 }
 
 const index = (donuts) => {
-    return donuts.map((donut) => donut.id + ' ' + donut.name + ' ' + donut.priceInCents);
+    return donuts.map((donut) => donut.id + ' ' + donut.name + ' ' + donut.priceInCents + ' ' + donut.inStock);
+}
+
+const edit = (donuts, id, name) => {
+    const index = donuts.findIndex((dn) => dn.id == id);
+    if (index > -1) {
+        const donut = donuts[index];
+        donut.name = name;
+        donut.priceInCents = donutPrices[name];
+        donut.inStock = true;
+        inform('Donut successfully updated');
+        return donuts;
+    } else {
+        inform('Donut not found. No action taken');
+        return donuts;
+    }
 }
 
 module.exports = {
