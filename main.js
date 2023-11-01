1c10a9827af9ef7df4f45b069c3f6a5981f6350f
Cris G <garciacristianw@gmail.com>
2023-11-01 01:39:07 +0000

    created a function that creates an article element for a donut and returns it

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 // create four additional article elements
-for (const i of '0123') {
-    const article = document.createElement('article');
-    article.innerHTML = `<h3>Donut ${Number(i) + 2}</h3>`;
+for (const donut of donuts) {
+    const article = createDonut(donut.name, donut.priceInCents, donut.inStock);
 
     const main = document.querySelector('main');
     main.append(article)
 }
 
-// const createDonut = (name, price, inStock, url) => {
-    
-// }
+const createDonut = (name, price, inStock, url="") => {
+    const article = document.createElement('article');
+    article.innerHTML = `<h3>${name}</h3>
+    <img style="width: 150px" src="${url}">
+    <h4>Price: ${price}<h4/>
+    <h4>In Stock: ${inStock}<h4/>`;
+
+    return article;
+}
