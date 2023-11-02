ae1e934ddea2ac9f48bdc91f030d3854f2019c9c
Cristian Garcia <crisg@sas.upenn.edu>
2023-11-02 10:13:33 -0400

    clear the form when the reset button is clicked

--- main.js
+++ main.js
@@ -3,18 +3,26 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const { donut, price, inStock, isVeganYes } = event.target;
+    const { donut, price, inStock, isVeganYes, calories } = event.target;
+
+    console.log(event.target);
 
     let isInStock = false;
     const inStockVal = inStock.value;
-    console.log(inStockVal);
     if (typeof inStockVal == 'string') {
         if (inStockVal == 'true') {
         isInStock = true;
         }
     };
 
-    generateDonut(donut.value, price.value, isInStock, isVeganYes.checked);
+    generateDonut(donut.value, price.value, isInStock, isVeganYes.checked, calories.value);
     
     form.reset();
 })
+
+// reset the form when the reset button is pressed
+form.addEventListener("reset"), (event) => {
+    event.preventDefault();
+
+    form.reset();
+}
